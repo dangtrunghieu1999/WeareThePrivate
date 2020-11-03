c5bca7305a8cfe0ce1340405d8104e9a26ad7738
Dang Trung Hieu <hieudt230899@gmail.com>
2020-11-03 17:54:23 +0700

    * Add folder

---
+++ WeaAreApp/WeaAreApp/Featuré/FavoriteProduct/ProductCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteViewController.swift
+//  WeaAreApp
+//
+//  Created by admin on 7/10/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Favorite"
+      
+    }
+    
+}
